7789c8bb8e454990e9ad91713bdf096cb2468f5c
hache231 <idrisnsimba46@gmail.com>
2024-05-14 16:06:46 +0100

    reorganizing my code, i'm made directories for models and views

---
+++ pharmacy/views/produits.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def list_products(request):
+    pass
+
+def add_products(request):
+    pass
+
+def modify_products(request):
+    pass
+
+def delete_products(request):
+    pass    
